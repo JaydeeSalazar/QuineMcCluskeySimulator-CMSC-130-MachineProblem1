db3142fc697d7950cbb8e2334b0c7d1574b12636
JaydeeSalazar <109349276+JaydeeSalazar@users.noreply.github.com>
2024-04-07 22:13:49 +0800

    BackEnd Something

---
+++ src/main/java/com/example/cmsc130_machineproblem1/Term.java
@@ -0,0 +1,19 @@
+package com.example.cmsc130_machineproblem1;
+
+public class Term
+{
+    private String mintermNumber;
+
+    private int mintermBinary;
+
+    private int ones_number;
+
+    public Term(mintermNumber, numberofVariables)
+    {
+        this.mintermNumber = mintermNumber;
+
+
+
+    }
+
+}
