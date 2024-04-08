00fe7f8950f24698baf318846c637754ad7c0fc9
JaydeeSalazar <109349276+JaydeeSalazar@users.noreply.github.com>
2024-04-08 20:59:52 +0800

    BackEnd Something

---
+++ src/main/java/com/example/cmsc130_machineproblem1/testingterm.java
@@ -0,0 +1,26 @@
+public class testingterm
+{
+    // Stores the minterm integers involved in forming the term
+    private ArrayList<Integer> groups;
+
+    // Stores the binary representation of the term
+    private String binaryRep;
+
+    // Stores the numbers of '1' in the term
+    private int onesNum;
+
+    // Constructor for the initialization of a brandnew term
+    // that consists of only one minterm
+
+    public testingterm(int minterm, int numOfVariables)
+    {
+        String binaryRep = Integer.toBinaryString(minterm);
+
+        //Add leading zeros in accordance to number of variables
+        //check if not needed
+
+        // Initialize the groups array/must contain the integer minterm
+
+        // Count number of ones
+    }
+}
